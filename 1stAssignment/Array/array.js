24a418bd3ec96a0f52bee6f3b04c1a235ce233f2
amanmagar <49756276+amanmagar@users.noreply.github.com>
2020-12-05 20:21:42 +0545

    1st_Assignment
    This contains all the code for first assignment of the bootcamp

--- 1stAssignment/Array/array.js
+++ 1stAssignment/Array/array.js
@@ -4,4 +4,3 @@ console.log("The removed item is "+ removed);
 arr.splice(7,2,"Dharan","Birtamod")
 console.log("The final array after adding is "+ arr)
 console.log("The length of the array is "+ arr.length)
-document.getElementById("")
